225cfed3160ebb0aa1e7de91b6b2e847bbf5cf9f
dkgusdkfk <dkgusdkfk@gmail.com>
2023-05-20 23:51:39 +0900

    메인화면(미완성)

--- frontend/src/views/AppMain.vue
+++ frontend/src/views/AppMain.vue
@@ -1,31 +1,222 @@
 <template>
-  <b-container class="bv-example-row mt-3 text-center">
-    <h3 class="underline-steelblue"><b-icon icon="house"></b-icon> SSAFY</h3>
-    <b-row>
-      <b-col></b-col>
-      <b-col cols="10">
-        <b-jumbotron bg-variant="muted" text-variant="dark" border-variant="dark">
-          <template #header>SSAFY Home</template>
+  <div>
+    <div class="intro intro-carousel swiper position-relative">
 
-          <template #lead>
-            슬기로운 싸피 생활 (:9기편👍)<br />
-            1학기동안 모두 고생 & 수고많으셨어요😍~~<br />
-            2학기 가서도 행복한 하루 ⭐️ 하루 되길 바랄께요.<br />
-            구~~~~~뤠~~~~~잇 9기 화이팅!!!.🌈<br />
-            그리고 각자 생각하는 곳에 취업!!! 꼭!꼭!꼭! 하세용.🐹
-          </template>
+      <div class="swiper-wrapper">
 
-          <hr class="my-4" />
+        <div class="swiper-slide carousel-item-a intro-item bg-image"
+          style="background-image: url(${root}/assets/img/Gwanghwamun.jpg)">
+          <div class="overlay overlay-a"></div>
+          <div class="intro-content display-table">
+            <div class="table-cell">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-8">
+                    <div class="intro-body">
+                      <p class="intro-title-top">
+                        Jong-ro, Seoul
+                        <!-- <br> 78345 -->
+                      </p>
+                      <h1 class="intro-title mb-4 ">
+                        <span class="color-b">03045 </span> <br> Gwanghwamun
+                      </h1>
+                      <p class="intro-subtitle intro-price">
+                        <a href="#"><span class="price-a">view more</span></a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide carousel-item-a intro-item bg-image"
+          style="background-image: url(${root}/assets/img/jamsil-tower.jpg)">
+          <div class="overlay overlay-a"></div>
+          <div class="intro-content display-table">
+            <div class="table-cell">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-8">
+                    <div class="intro-body">
+                      <p class="intro-title-top">
+                        Song-pa, Seoul <br> 05556
+                      </p>
+                      <h1 class="intro-title mb-4">
+                        <span class="color-b">300 </span> <br> Olympic-ro
+                      </h1>
+                      <p class="intro-subtitle intro-price">
+                        <a href="#"><span class="price-a">view more</span></a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide carousel-item-a intro-item bg-image"
+          style="background-image: url(${root}/assets/img/Suwon-Hwaseong.jpg)">
+          <div class="overlay overlay-a"></div>
+          <div class="intro-content display-table">
+            <div class="table-cell">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-8">
+                    <div class="intro-body">
+                      <p class="intro-title-top">
+                        Suwon, Gyeonggi <br> 16255
+                      </p>
+                      <h1 class="intro-title mb-4">
+                        <span class="color-b">21 </span> <br> Changnyong-daero
+                      </h1>
+                      <p class="intro-subtitle intro-price">
+                        <a href="#"><span class="price-a">view more</span></a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+    <!-- End Intro Section -->
+    <c:if test="${not empty recommendAttraction}">
+      <section class="section-property section-t8">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="title-wrap d-flex justify-content-between">
+                <div class="title-box">
+                  <h2 class="title-a">우리 지역 여행지 추천</h2>
+                </div>
+                <div class="title-link">
+                  <a href="${root}/trip/recommend">더 보기 <span class="bi bi-chevron-right"></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
 
-          <p>Vue + Bootstrap활용.</p>
-          <p>Bootstrap-vue는 버전 <b>4.6.1</b>을 권장합니다.</p>
-          <p><b>BoardList.vue</b>를 바꿔가면서 테스트하세요.</p>
-          <p>Bootstrap의 <b>table</b> 사용법을 익히게됩니다.</p>
-        </b-jumbotron>
-      </b-col>
-      <b-col></b-col>
-    </b-row>
-  </b-container>
+          <div id="property-carousel" class="swiper">
+            <div class="swiper-wrapper">
+
+              <c:forEach var="attraction" items="${recommendAttraction}">
+                <div class="carousel-item-b swiper-slide">
+                  <div class="card-box-a card-shadow">
+                    <div class="img-box-a" style="height: 480px;">
+                      <img src="${attraction.firstImage}" alt="" class="img-a img-fluid">
+                    </div>
+                    <div class="card-overlay">
+                      <div class="card-overlay-a-content">
+                        <div class="card-header-a">
+                          <h2 class="card-title-a">
+                            <a href="property-single.jsp">
+                              ${attraction.title}
+                            </a>
+                          </h2>
+                        </div>
+                        <div class="card-body-a">
+                          <div class="price-box d-flex">
+                            <span class="price-a">주소</span>
+                          </div>
+                          <div style="color: white">${attraction.address1} ${attraction.address2}</div>
+                          <a href="#" class="link-a">Click here to view <span class="bi bi-chevron-right"></span>
+                          </a>
+                        </div>
+                        <div class="card-footer-a">
+                          <ul class="card-info d-flex justify-content-around">
+                            <li>
+                              <h4 class="card-info-title">Area</h4> <span>340m <sup>2</sup>
+                              </span>
+                            </li>
+                            <li>
+                              <h4 class="card-info-title">Beds</h4> <span>2</span>
+                            </li>
+                            <li>
+                              <h4 class="card-info-title">Baths</h4> <span>4</span>
+                            </li>
+                            <li>
+                              <h4 class="card-info-title">Garages</h4> <span>1</span>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </c:forEach>
+              <!-- End carousel item -->
+            </div>
+          </div>
+          <div class="propery-carousel-pagination carousel-pagination"></div>
+
+        </div>
+      </section>
+    </c:if>
+    <!-- End Latest Properties Section --> <!-- ======= Latest News Section ======= -->
+    <section class="section-news section-t8">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="title-wrap d-flex justify-content-between">
+              <div class="title-box">
+                <h2 class="title-a">Hot Place</h2>
+              </div>
+              <div class="title-link">
+                <a href="${root}/trip/hotPlace">더보기 <span class="bi bi-chevron-right"></span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="news-carousel" class="swiper">
+          <div class="swiper-wrapper">
+
+            <c:forEach var="hotPlace" items="${hotPlaceList}" begin="0" end="10">
+              <div class="carousel-item-c swiper-slide">
+                <div class="card-box-b card-shadow news-box">
+                  <div class="img-box-b" style="height: 280px">
+                    <c:choose>
+                      <c:when test="${not empty hotPlace.imgPath}">
+                        <img src="${hotPlace.imgPath}" alt="" class="img-b img-fluid">
+                      </c:when>
+                      <c:otherwise>
+                        <img src="${root}/assets/img/noimg.gif" alt="" class="img-b img-fluid">
+                      </c:otherwise>
+                    </c:choose>
+                  </div>
+                  <div class="card-overlay">
+                    <div class="card-header-b">
+                      <div class="card-category-b">
+                        <a class="category-b">♥ ${hotPlace.count}</a>
+                      </div>
+                      <div class="card-title-b">
+                        <h2 class="title-2">
+                          <a>${hotPlace.title}</a>
+                        </h2>
+                      </div>
+                      <div class="card-date">
+                        <span class="date-b">${hotPlace.address1} ${hotPlace.address2}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- End carousel item -->
+            </c:forEach>
+          </div>
+        </div>
+
+        <div class="news-carousel-pagination carousel-pagination"></div>
+      </div>
+    </section>
+
+  </div>
 </template>
 
 <script>
