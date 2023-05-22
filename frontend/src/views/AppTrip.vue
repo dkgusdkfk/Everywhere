88b5b4ddf2c7efc234f87811d79a447b5036ec8b
kimkiheon <cbj2741@gmail.com>
2023-05-22 15:05:07 +0900

    env 변수 사용

--- frontend/src/views/AppTrip.vue
+++ frontend/src/views/AppTrip.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div class="bv-example-row text-center">
         <router-view></router-view>
     </div>
@@ -10,17 +9,3 @@ export default {
 };
 </script>
 <style></style>
-  
-=======
-  <div class="bv-example-row text-center">
-    <router-view></router-view>
-  </div>
-</template>
-<script>
-export default {
-  name: "AppTrip",
-};
-</script>
-<style>
-</style>
->>>>>>> 99f653fa1b46d734a7366f886301564f9716d196
