ec7ffc617f12ad70535843d048f436144e2ecfc0
백아현 <dkgusdkfk@gmail.com>
2023-05-25 17:16:58 +0900

    비밀번호찾기

--- frontend/src/components/user/FindPassword.vue
+++ frontend/src/components/user/FindPassword.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <section class="intro-single">
-            <div class="container">
+            <div>
                 <div class="row">
                     <div class="col-md-12 col-lg-8">
                         <div class="title-single-box">
@@ -21,60 +21,63 @@
                 </div>
             </div>
         </section>
-        <!-- End Intro Single--> <!-- ======= Property Grid ======= -->
-        <!-- <c:choose>
-            <c:when test="${empty findUser}">
-                <div class="container">
-                    <div class="input-form-backgroud row">
-                        <div class="input-form col-md-12 mx-auto">
-                            <form action="${root}/user/findPassword" method="post">
-                                <div class="mb-3">
-                                    <label for="id">아이디</label> <input type="text"
-                                        class="form-control" id="id" name="id"
-                                        placeholder="id를 입력해주세요" required>
-                                    <div class="invalid-feedback">id를 입력해주세요.</div>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="email">이메일</label> <input type="email"
-                                        class="form-control" id="email" name="email"
-                                        placeholder="${findUser.email}" required>
-                                    <div class="invalid-feedback">이메일을 입력해주세요.</div>
-                                </div>
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">확인하기</button>
-                            </form>
+            <div v-if="!this.findUser">
+                <div class="input-form-backgroud row">
+                    <div class="input-form col-md-12 mx-auto">
+                        <div class="mb-3">
+                            <label for="id">아이디</label> <input type="text"
+                                class="form-control" id="id" name="id"
+                                placeholder="id를 입력해주세요" v-model="id" required>
                         </div>
+                        <div class="mb-3">
+                            <label for="email">이메일</label> <input type="email"
+                                class="form-control" id="email" name="email"
+                                placeholder="이메일을 입력해주세요" v-model="email" required>
+                        </div>
+                        <button class="btn btn-primary btn-lg btn-block" @click="find">확인하기</button>
                     </div>
                 </div>
-            </c:when>
-            <c:otherwise>
-                <div class="container">
-                    <div class="input-form-backgroud row">
-                        <div class="input-form col-md-12 mx-auto">
-                            <h4 class="title-2 mt-4">
-                                <strong>ID: </strong> <span class="color-text-a">${findUser.id}</span>
-                            </h4>
-                            <h4 class="title-2 mt-4">
-                                <strong>PW: </strong> <span class="color-text-a">${findUser.password}</span>
-                            </h4>
-                        </div>
+            </div>
+
+            <div v-else>
+                <div class="input-form-backgroud row">
+                    <div class="input-form col-md-12 mx-auto">
+                        <h4 class="title-2 mt-4">
+                            <strong>ID: </strong> <span class="color-text-a">{{findUser.id}}</span>
+                        </h4>
+                        <h4 class="title-2 mt-4">
+                            <strong>PW: </strong> <span class="color-text-a">{{findUser.password}}</span>
+                        </h4>
                     </div>
                 </div>
-            </c:otherwise>
-        </c:choose> -->
+            </div>
     </div>
 </template>
 
 <script>
+import http from "@/api/http";
+
 export default {
     name: 'FindPassword',
     components: {},
     data() {
         return {
-            message: '',
+            findUser: null,
+            id: null,
+            email: null,
         };
     },
     created() {},
-    methods: {},
+    methods: {
+        find() {
+            http.post(`/user/find`, {
+                id: this.id,
+                email: this.email,
+            }).then(({ data }) => {
+                this.findUser = data;
+            });
+        }
+    },
 };
 </script>
 
