4e0e2f697dafca4d451b571c2bb387d5f7e86417
kimkiheon <cbj2741@gmail.com>
2023-05-25 17:54:51 +0900

    docs

--- docs/Everywhere DDL.sql
+++ docs/Everywhere DDL.sql
@@ -1,3 +1,4 @@
+
 create database everywhere;
 use everywhere;
 
@@ -15,52 +16,6 @@ CREATE TABLE `gugun` (
   PRIMARY KEY (`gugun_code`,`sido_code`),
   KEY `gugun_to_sido_sido_code_fk_idx` (`sido_code`),
   CONSTRAINT `gugun_to_sido_sido_code_fk` FOREIGN KEY (`sido_code`) REFERENCES `sido` (`sido_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE `attraction_info` (
-  `content_id` int NOT NULL,
-  `content_type_id` int DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL,
-  `addr1` varchar(100) DEFAULT NULL,
-  `addr2` varchar(50) DEFAULT NULL,
-  `zipcode` varchar(50) DEFAULT NULL,
-  `tel` varchar(50) DEFAULT NULL,
-  `first_image` varchar(200) DEFAULT NULL,
-  `first_image2` varchar(200) DEFAULT NULL,
-  `readcount` int DEFAULT NULL,
-  `sido_code` int DEFAULT NULL,
-  `gugun_code` int DEFAULT NULL,
-  `latitude` decimal(20,17) DEFAULT NULL,
-  `longitude` decimal(20,17) DEFAULT NULL,
-  `mlevel` varchar(2) DEFAULT NULL,
-  PRIMARY KEY (`content_id`),
-  KEY `attraction_to_sido_code_fk_idx` (`sido_code`),
-  KEY `attraction_to_gugun_code_fk_idx` (`gugun_code`),
-  CONSTRAINT `attraction_to_gugun_code_fk` FOREIGN KEY (`gugun_code`) REFERENCES `gugun` (`gugun_code`),
-  CONSTRAINT `attraction_to_sido_code_fk` FOREIGN KEY (`sido_code`) REFERENCES `sido` (`sido_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE `attraction_description` (
-  `content_id` int NOT NULL,
-  `homepage` varchar(100) DEFAULT NULL,
-  `overview` varchar(10000) DEFAULT NULL,
-  `telname` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`content_id`),
-  CONSTRAINT `attraction_detail_to_attraciton_id_fk` FOREIGN KEY (`content_id`) REFERENCES `attraction_info` (`content_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `attraction_detail` (
-  `content_id` int NOT NULL,
-  `cat1` varchar(3) DEFAULT NULL,
-  `cat2` varchar(5) DEFAULT NULL,
-  `cat3` varchar(9) DEFAULT NULL,
-  `created_time` varchar(14) DEFAULT NULL,
-  `modified_time` varchar(14) DEFAULT NULL,
-  `booktour` varchar(5) DEFAULT NULL,
-  PRIMARY KEY (`content_id`),
-  CONSTRAINT `attraction_detail_to_basic_content_id_fk` FOREIGN KEY (`content_id`) REFERENCES `attraction_info` (`content_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -150,7 +105,6 @@ CREATE TABLE `qna` (
   `content` varchar(10000) NOT NULL,
   `hit` int DEFAULT 0,
   `register_time` datetime DEFAULT current_timestamp(),
-  `qnacol` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`qna_id`),
   KEY `user_id_pk_idx` (`user_id`),
   CONSTRAINT `user_id_pk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
