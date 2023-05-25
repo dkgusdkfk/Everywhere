6a20283ddf092f4aa1999c3b794ee6a13a47d203
백아현 <dkgusdkfk@gmail.com>
2023-05-25 09:27:13 +0900

    crossorigin 추가

--- backend/src/main/java/com/ssafy/enjoytrip/trip/controller/TripRestController.java
+++ backend/src/main/java/com/ssafy/enjoytrip/trip/controller/TripRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/trip")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"*"}, maxAge = 6000)
 public class TripRestController {
     private final TripService tripService;
     private static final String SUCCESS = "success";
